aa9be0f58176fc8e97938b31a8fc3b8570e920c8
Ákos Kiss <ak@coram.pub>
2023-07-11 07:07:40 +0200

    Use local package nickname for testing

--- ginkgo-time-test.lisp
+++ ginkgo-time-test.lisp
@@ -1,7 +1,8 @@
 ;;;; ginkgo-time-test.lisp
 
 (defpackage #:ginkgo-time-test
-  (:use #:cl #:fiveam))
+  (:use #:cl #:fiveam)
+  (:local-nicknames (:gt :ginkgo-time)))
 
 (in-package #:ginkgo-time-test)
 
@@ -11,32 +12,32 @@
 (test instant-now
   (let ((unix-time (- (get-universal-time)
                       trivial-clock:+universal-time-epoch-offset+))
-        (now (ginkgo-time:instant-now)))
-    (is (<= unix-time (ginkgo-time:instant-to-epoch-second now))))
-  (let* ((ginkgo-time:*clock-now* (list 0 0))
-         (i (ginkgo-time:instant-now)))
-    (ginkgo-time:instant= i ginkgo-time:*epoch-instant*))
-  (let* ((ginkgo-time:*clock-now-function* (lambda () (values 0 0)))
-         (i (ginkgo-time:instant-now)))
-    (ginkgo-time:instant= i ginkgo-time:*epoch-instant*)))
+        (now (gt:instant-now)))
+    (is (<= unix-time (gt:instant-to-epoch-second now))))
+  (let* ((gt:*clock-now* (list 0 0))
+         (i (gt:instant-now)))
+    (gt:instant= i gt:*epoch-instant*))
+  (let* ((gt:*clock-now-function* (lambda () (values 0 0)))
+         (i (gt:instant-now)))
+    (gt:instant= i gt:*epoch-instant*)))
 
 (test instant-epoch-second-conversion
-  (let ((i1 (ginkgo-time:instant-of-epoch-second 3 1))
-        (i2 (ginkgo-time:instant-of-epoch-second 4 -999999999))
-        (i3 (ginkgo-time:instant-of-epoch-second 2 1000000001)))
-    (is (ginkgo-time:instant= i1 i2))
-    (is (ginkgo-time:instant= i2 i3))
+  (let ((i1 (gt:instant-of-epoch-second 3 1))
+        (i2 (gt:instant-of-epoch-second 4 -999999999))
+        (i3 (gt:instant-of-epoch-second 2 1000000001)))
+    (is (gt:instant= i1 i2))
+    (is (gt:instant= i2 i3))
     (loop :for i :in (list i1 i2 i3)
-          :do (is (eql 3 (ginkgo-time:instant-to-epoch-second i))))))
+          :do (is (eql 3 (gt:instant-to-epoch-second i))))))
 
 (test instant-epoch-milli-conversion
   (let* ((epoch-milli 54321)
-         (i (ginkgo-time:instant-of-epoch-milli epoch-milli)))
-    (is (eql epoch-milli (ginkgo-time:instant-to-epoch-milli i)))))
+         (i (gt:instant-of-epoch-milli epoch-milli)))
+    (is (eql epoch-milli (gt:instant-to-epoch-milli i)))))
 
 (test instant-plus-duration
-  (let ((d (ginkgo-time:plus (ginkgo:chrono-unit-duration :days)
-                             (ginkgo:chrono-unit-duration :nanos))))
-    (is (ginkgo-time:instant= (ginkgo-time:instant-of-epoch-second
-                               ginkgo-time:+seconds-per-day+ 1)
-                              (ginkgo:plus ginkgo-time:*epoch-instant* d)))))
+  (let ((d (gt:plus (ginkgo:chrono-unit-duration :days)
+                    (ginkgo:chrono-unit-duration :nanos))))
+    (is (gt:instant= (gt:instant-of-epoch-second
+                      gt:+seconds-per-day+ 1)
+                     (ginkgo:plus gt:*epoch-instant* d)))))
