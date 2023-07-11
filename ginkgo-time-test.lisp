706b02d71241ee60213503271acb63ee73269daa
Ákos Kiss <ak@coram.pub>
2023-07-11 06:37:23 +0200

    Export more symbols, add some basic tests

--- ginkgo-time-test.lisp
+++ ginkgo-time-test.lisp
@@ -8,5 +8,22 @@
 (def-suite :ginkgo-time)
 (in-suite :ginkgo-time)
 
-(test instant
-  (finishes (ginkgo-time:instant-now)))
+(test instant-now
+  (let ((unix-time (- (get-universal-time)
+                      trivial-clock:+universal-time-epoch-offset+))
+        (now (ginkgo-time:instant-now)))
+    (is (<= unix-time (ginkgo-time:instant-to-epoch-second now)))))
+
+(test instant-epoch-second-conversion
+  (let ((i1 (ginkgo-time:instant-of-epoch-second 3 1))
+        (i2 (ginkgo-time:instant-of-epoch-second 4 -999999999))
+        (i3 (ginkgo-time:instant-of-epoch-second 2 1000000001)))
+    (is (ginkgo-time:instant= i1 i2))
+    (is (ginkgo-time:instant= i2 i3))
+    (loop :for i :in (list i1 i2 i3)
+          :do (is (eql 3 (ginkgo-time:instant-to-epoch-second i))))))
+
+(test instant-epoch-milli-conversion
+  (let* ((epoch-milli 54321)
+         (i (ginkgo-time:instant-of-epoch-milli epoch-milli)))
+    (is (eql epoch-milli (ginkgo-time:instant-to-epoch-milli i)))))
--- package.lisp
+++ package.lisp
@@ -4,6 +4,25 @@
   (:nicknames #:ginkgo)
   (:use #:cl)
   (:export
+
+   ;; const.lisp
+   +hours-per-day+
+   +minutes-per-hour+
+   +minutes-per-day+
+   +seconds-per-minute+
+   +seconds-per-hour+
+   +seconds-per-day+
+   +millis-per-second+
+   +millis-per-day+
+   +micros-per-second+
+   +micros-per-day+
+   +nanos-per-micro+
+   +nanos-per-milli+
+   +nanos-per-second+
+   +nanos-per-minute+
+   +nanos-per-hour+
+   +nanos-per-day+
+
    ;; generic.lisp
    #:plus
    #:to-string
@@ -32,4 +51,12 @@
    ;; instant.lisp
    #:*epoch-instant*
    #:instant=
-   #:instant-now))
+   #:instant-now
+   #:instant-of-epoch-second
+   #:instant-of-epoch-milli
+   #:instant-to-epoch-second
+   #:instant-to-epoch-milli
+
+   ;; Misc. accessors
+   seconds
+   nanos))
