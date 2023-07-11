0d9571dee29d5e6409ff141355a0b166fcd80feb
Ákos Kiss <ak@coram.pub>
2023-07-11 06:10:42 +0200

    Implement adding durations to instants, add epoch conversion functions

--- instant.lisp
+++ instant.lisp
@@ -17,7 +17,16 @@
 (declaim (type instant *epoch-instant*)
          (ftype (function (instant instant) boolean) instant=)
          (ftype (function () (values instant &optional))
-                instant-now))
+                instant-now)
+         (ftype (function (integer &optional integer)
+                          (values instant &optional))
+                instant-of-epoch-second)
+         (ftype (function (integer)
+                          (values instant &optional))
+                instant-of-epoch-milli)
+         (ftype (function (instant) (values integer &optional))
+                instant-to-epoch-second
+                instant-to-epoch-milli))
 
 (defparameter *epoch-instant* (make-instance 'instant :seconds 0))
 
@@ -37,3 +46,31 @@
     (make-instance 'instant
                    :seconds seconds
                    :nanos nanos)))
+
+(defmethod plus ((i instant) (d duration))
+  (multiple-value-bind (seconds-carry nanos)
+      (floor (+ (nanos i) (nanos d)) +nanos-per-second+)
+    (make-instance 'instant
+                   :seconds (+ (seconds i) (seconds d) seconds-carry)
+                   :nanos nanos)))
+
+(defun instant-of-epoch-second (epoch-second &optional nano-adjustment)
+  (if nano-adjustment
+      (multiple-value-bind (seconds-carry nanos)
+          (floor nano-adjustment +nanos-per-second+)
+        (make-instance 'instant :seconds (+ epoch-second seconds-carry)
+                                :nanos nanos))
+      (make-instance 'instant :seconds epoch-second)))
+
+(defun instant-of-epoch-milli (epoch-milli)
+  (multiple-value-bind (seconds millis)
+      (floor epoch-milli +millis-per-second+)
+    (make-instance 'instant :seconds seconds
+                            :nanos (* millis +nanos-per-milli+))))
+
+(defun instant-to-epoch-second (instant)
+  (seconds instant))
+
+(defun instant-to-epoch-milli (instant)
+  (+ (* (seconds instant) +millis-per-second+)
+     (floor (nanos instant) +nanos-per-milli+)))
